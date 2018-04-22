d1f0d6d7149bb864c7ec00a3e00575edbaa4ec30
John Doe <cormil@gmail.com>
2018-04-22 16:43:16 -0500

    v1 of Systems 2 hw2

---
+++ Systems2/hw2/safe.c
@@ -0,0 +1,85 @@
+#include	"safeBustersHeaders.h"
+
+int continueRunning = 1;
+int safeOpened = 0;
+int comboNum1;
+int comboNum2;
+int comboNum3;
+
+void sigIntHandler (int sigNum
+		   )
+{
+	continueRunning = 0;
+}
+
+int		computeResponse	(int*		countPtr
+				)
+{
+  int	toReturn;
+
+  sleep(1);
+  (*countPtr)--;
+
+  if  (*countPtr == 0)
+  {
+    printf("Safe \"Click!\"\n");
+    toReturn	= SIG_RIGHT_DIGIT;
+  }
+  else
+  {
+    printf("Safe \"Nope.\"\n");
+    toReturn	= SIG_WRONG_DIGIT;
+  }
+
+  return(toReturn);
+}
+
+void sigTryNextDigitHandler (int sigNum, siginfo_t* infoPtr, void* dataPtr
+			    )
+{
+	if (comboNum1 > 0)
+	{
+		kill(infoPtr->si_pid,computeResponse(&comboNum1));
+	}
+	else if (comboNum2 > 0)
+	{
+		kill(infoPtr->si_pid,computeResponse(&comboNum2));
+	}
+	else if(comboNum3 > 0)
+	{
+		if ((computeResponse(&comboNum3)) == SIG_RIGHT_DIGIT)
+		{
+			safeOpened = 1;
+		}
+		kill(infoPtr->si_pid,computeResponse(&comboNum3));
+		continueRunning = 0;
+	}
+}
+
+int main () {
+	
+	srand(getpid());
+	struct sigaction	act;
+	act.sa_flags = SA_SIGINFO;
+       	act.sa_sigaction = sigTryNextDigitHandler;
+	sigaction(SIG_TRY_NEXT_DIGIT, &act, NULL);
+	signal(SIG_QUIT, sigIntHandler);
+
+	comboNum1 = (rand() % 16) + 1;
+	comboNum2 = (rand() % 16) + 1;
+	comboNum3 = (rand() % 16) + 1;
+	printf("Combo: %d-%d-%d ", comboNum1, comboNum2, comboNum3);
+
+	while (continueRunning)
+		sleep(1);
+
+	if (safeOpened == 1)
+	{
+		printf("Safe:  I am no match for the thief's skill! \n");
+	}
+	else 
+	{
+		printf("Safe:  Go back to safe cracking school, thief! \n");
+	}
+	return(EXIT_SUCCESS);
+}
